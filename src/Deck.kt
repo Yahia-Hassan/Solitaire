1cd5cea2baeebb4d48d7998e94154f887f0f5a2a
Yahia Hassan <yehia4747@gmail.com>
2018-07-22 04:58:30 +0200

    Add the Deck.

---
+++ src/Deck.kt
@@ -0,0 +1,11 @@
+class Deck {
+
+    val cards = Array(52) { Card(it % 13, getSuit(it)) }
+
+    private fun getSuit(i: Int) = when(i / 13) {
+            0 -> "Clubs"
+            1 -> "Diamonds"
+            2 -> "Hearts"
+            else -> "Spades"
+    }
+}
